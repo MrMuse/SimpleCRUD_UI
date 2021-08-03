673edac64699ea2ca4ff9bd6da54b4f4a488f2b5
MrMuse <mailsmustafa@gmail.com>
2021-08-03 16:08:30 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#app.py
+from flask import Flask, render_template, request, redirect, url_for, flash
+ 
+app = Flask(__name__)
+app.secret_key = "pass"
+ 
+@app.route('/')
+def Index():
+    return render_template('index.html')
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
